f318e93288f6480cb884f5d0e5c537cd9c6f31ac
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-16 21:01:02 -0500

    a thing works

---
+++ controllers/devPoolController.js
@@ -0,0 +1,10 @@
+//Dependencies
+const express = require('express');
+const router = express.Router();
+
+//Import model (the model from vue.js will need to go here);
+
+//----------------------------------------
+
+
+
