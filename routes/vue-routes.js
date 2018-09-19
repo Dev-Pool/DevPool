56f9ac2427a138bc4e7b2622b1aae5cc7d388353
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-18 19:18:03 -0500

    Hey, our stuff works....now just for the hard part (vue.js)

---
+++ routes/vue-routes.js
@@ -0,0 +1,10 @@
+//Dependencies 
+const path = require("path");
+
+//Routes
+module.exports = app => {
+    app.get("/", (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+}
---
+++ views/app.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div id="app">
+    <Navbar/>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components:{
+    Navbar
+  }
+}
+</script>
+
+<style>
+</style>
