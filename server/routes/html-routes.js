65933ae5281e7ef7e905a4c67e233920b30772d4
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-22 11:35:36 -0500

    restructuring code

--- server/routes/html-routes.js
+++ server/routes/html-routes.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 //routes
 module.exports = app => {
-    //html file "index" in layouts
+    //html file "index" in client
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../layouts/index.html'));
+        res.sendFile(path.join(__dirname, '../../client/index.html'));
 
     });
 
     //html file for Corey's linkedin route
     app.get('/success', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../layouts/views/success.html'))
+        res.sendFile(path.join(__dirname, '../../client/success.html'))
     });
 }; 
