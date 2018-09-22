de6595679a4895d1b23b1ef5690d9e2cd66bbcb1
corey-mitchell <corey69_96@yahoo.com>
2018-09-22 16:16:11 -0500

    had to get up to date with master branch

--- server/server.js
+++ server/server.js
@@ -1,28 +1,24 @@
 //dependencies for app
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 //setting up express app
 const app = express();
 let PORT = process.env.PORT || 9090;
-
 //requiring models
+
 const db = require('./models');
 
 //setting up Express app to handle data parsing
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(bodyParser.json());
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-//we'll need to require the routes here
-
-const routes = require("./routes/vue-routes.js");
-app.use(routes);
-
+//Requiring routes for the server
+require("./routes/html-routes.js")(app);
 //-----------------------------------
 
 //syncing sequelize models and starting Express
-db.sequelize.sync().then(()=>{
+db.sequelize.sync({}).then(()=>{
     app.listen(PORT, ()=>{
         console.log(`App listening on PORT ${PORT}`);
     });
