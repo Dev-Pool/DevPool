d314e66b2c08d2fc970bf6290db9db13512545f4
corey-mitchell <corey69_96@yahoo.com>
2018-09-23 14:08:46 -0500

    finally got back the token! now to add some ajax calls and we're set!

--- server/public/javascript/app2.js
+++ server/public/javascript/app2.js
@@ -4,24 +4,33 @@ window.addEventListener('load', function () {
     const authCode = window.location.search.substr(1);
 
     // Passes auth code thru to linkedin url to get access token.
-    const tokenURL = `https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&${authCode}&redirect_uri=http://localhost:9090&client_id=78k0z5io5whh5e&client_secret=rhqY2Mgy6OhOnS0Q`;
+    const tokenURL = `https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&${authCode}&redirect_uri=http://localhost:9090/success&client_id=78k0z5io5whh5e&client_secret=rhqY2Mgy6OhOnS0Q`;
 
     // Ajax call to linkedin for access token.
+    // Fixed CORS issues by prepending the url line
     $.ajax({
-        url: tokenURL,
         method: 'POST',
-        Type: 'application/x-www-form-urlencoded',
-        dataType: 'json'
+        url: `https://cors-escape.herokuapp.com/${tokenURL}`,
+        headers: {
+        "Access-Control-Allow-Origin": "*"
+        }
     }).then((res)=>{
-        console.log(res);
+        // console.log(res.access_token, res.expires_in);
+
+        // Storing the object in the local storage. For now!
+        localStorage.setItem('Access_token', res.access_token);
+        localStorage.setItem('Expires_in', res.expires_in);
+
     })
 
     // Will need to store the tokens somewhere for easy access.
+    // As of right now, they are stored in localStorage above. But that isn't safe.
+    // We need to place them in the database or somewhere on the server.
 
 
     // =======================================================
 
     // Closes window once everything is done.
-    // window.close();
+    window.close();
 
 });
