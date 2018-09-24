82259e69d1b72f58ae8ccf83bfd554ad021ff8f0
corey-mitchell <corey69_96@yahoo.com>
2018-09-24 00:07:02 -0500

    fixed a few files

--- client/javascript/app2.js
+++
@@ -1,8 +0,0 @@
-console.log("window loaded")
-
-
-const newURL = new URL(`https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&${localStorage.getItem('linkedinAPIauthcode')}&redirect_uri=http://localhost:9090&client_id=78k0z5io5whh5e&client_secret=rhqY2Mgy6OhOnS0Q`);
-
-const tokenExpiry = newURL.searchParams.get("expires_in");
-
-console.log(tokenExpiry);
