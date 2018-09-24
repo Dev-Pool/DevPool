4a2db3db61375b5f20b490a80b65038ab2a05edf
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-24 14:07:00 -0500

    made the front end a thing

--- client/src/App.vue
+++ client/src/App.vue
@@ -15,13 +15,5 @@ export default {
 
 <style>
  @import './assets/css/dev-pool.css';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
 </style>
