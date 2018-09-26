db3722010a63686580d8ff0cb9470bd48873b8f1
corey-mitchell <corey69_96@yahoo.com>
2018-09-25 19:00:29 -0500

    everything runs. may need to do some css stuffs. must run both server and client portsgit add .!

--- server/routes/html-routes.js
+++ server/routes/html-routes.js
@@ -5,11 +5,11 @@ const path = require("path");
 module.exports = app => {
     // html file "index" in client
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '/index.html'));
+        res.sendFile(path.join(__dirname, '../../client/index.html'));
     });
 
     //html file for Corey's linkedin route
     app.get('/success', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../client/success.html'))
+        res.sendFile(path.join(__dirname, '../../client/success.html'));
     });
 }; 
