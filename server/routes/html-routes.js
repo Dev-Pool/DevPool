44eb0d462a945b06cf81344b5d5e897d10f142b6
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-26 19:25:50 -0500

    stashing to pull

--- server/routes/html-routes.js
+++ server/routes/html-routes.js
@@ -9,16 +9,8 @@ module.exports = app => {
         res.sendFile(path.join(__dirname, '../../client/index.html'));
     });
 
-<<<<<<< HEAD
-    // html file for Corey's linkedin route
-    app.get('/success', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../client/success.html'));
-    });
+    // //html file for Linkedin route
+    // app.get('/success', (req, res) => {
+    //     res.sendFile(path.join(__dirname, '../../client/success.html'));
+    // });
 }; 
-=======
-    //html file for Linkedin route
-    app.get('/success', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../client/success.html'));
-    });
-}; 
->>>>>>> 87fa08ccc8c7717a3d59ec2f42b816b9f1089d6d
