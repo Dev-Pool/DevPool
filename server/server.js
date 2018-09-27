230466f0576e8c74a3d733abae5fb1b5d5ca136c
corey-mitchell <corey69_96@yahoo.com>
2018-09-27 00:13:52 -0500

    sign up form successfully pushes user info into DB.

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ require("./routes/html-routes.js")(app);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('../client/build'));
+    app.use(express.static('client/build'));
   
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
